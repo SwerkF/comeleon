9af2bc707c658f46fc3e326135d7c37f47a10297
Clement <clementduchene0803@gmail.com>
2022-09-16 17:34:16 +0200

    Initial Commit

---
+++ src/Entity/Presentation.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PresentationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PresentationRepository::class)
+ */
+class Presentation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $activites;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getActivites(): ?string
+    {
+        return $this->activites;
+    }
+
+    public function setActivites(string $activites): self
+    {
+        $this->activites = $activites;
+
+        return $this;
+    }
+}
