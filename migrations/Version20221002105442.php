7fd6e260307b973e9456ed46e679d271bb11181b
Oliwer Skweres <swerk.pro@gmail.com>
2022-10-04 00:37:57 +0200

    <Feat>Refonte gestion des prestations
    La BDD a été modifié, je devais donc changer le menu des prestations.

--- migrations/Version20221002105442.php
+++ migrations/Version20221002105442.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220930145158 extends AbstractMigration
+final class Version20221002105442 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20220930145158 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE avis DROP nom, DROP prenom');
-        $this->addSql('ALTER TABLE prestations ADD prix INT NOT NULL, CHANGE description image VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE prestations CHANGE prix prix VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE avis ADD nom VARCHAR(255) NOT NULL, ADD prenom VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE prestations DROP prix, CHANGE image description VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE prestations CHANGE prix prix TEXT NOT NULL');
     }
 }
