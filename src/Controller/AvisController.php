f20da5d435eac355471051937cdab603c5553eeb
Clement <clementduchene0803@gmail.com>
2022-10-05 15:09:09 +0200

    Auto stash before merge of "feature" and "origin/feature"

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 use App\Entity\Avis;
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,25 +17,40 @@ class AvisController extends AbstractController
      */
     public function index(): Response
     {
-    $repo=$this->getDoctrine()->getRepository(Avis::Class);
 
+        $repo = $this->getDoctrine()->getRepository(Avis::class);
     $avis=$repo->findAll();
+    $avis = new Avis();
+
+        $form = $this->createFormBuilder($avis)
+        ->add('nom',TextType::class)
+        ->add('prenom',TextType::class)
+        ->add("commentaire",TextType::class)
+        ->add('save', SubmitType::class, array('label'=>'Enregistrer',
+        'attr'=>array('class'=>'btn')
+           
+        ))
+        ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $avis = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($avis);
+            $entityManager->flush();
+            return $this->redirect('app_avis');
+
+           
+        }
         return $this->render('avis/index.html.twig', [
             'controller_name' => 'AvisController',
-            'avis'=>$avis
+            'avis' => $avis,
+            'form'=>$form
         ]);
     }
     /**
-     * @Route("/create_avis", name="app_avis")
+     * @Route("/create_avis", name="app_avis_create")
      */
-     /*
-    public function createNewAvis()
-    {
-       $avis=new Avis();
-       $avis->setNom('');
-       $avis->setPrenom('');
-       $avis->setCommentaire('');
-    */
-}
     
+   
 
+}
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +13,33 @@ class ContactController extends AbstractController
      */
     public function index(): Response
     {
+        $contact = $this->getDoctrine()->getRepository(Contact::class)->findAll();
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+            'contact'=>$contact
         ]);
     }
+    public function addContact(Request $request) {
+        
+        $contact = new Contact();
+
+        $formContact= $this->createFormBuilder($contact)
+
+        ->add("commentaire",TextType::class)
+        ->add('save', SubmitType::class, array('label'=>'Ajouter',
+        'attr'=>array('class'=>'btn')
+           
+        ))
+        ->getForm();
+        $formContact->handleRequest($request);
+        if ($formContact->isSubmitted() && $formContact->isValid()) {
+            $avis = $formContact->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($avis);
+            $entityManager->flush();
+            return $this->redirect('app_avis');
+
+           
+        }
+}
 }
